f0b27f56e43bb93e6e974b6a9e2c7ab27f6e9bc4
benlaguroun <benlaguroun@gmail.com>
2024-04-08 21:46:38 +0000

    0. N queens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+import sys
+
+def is_safe(board, row, col):
+    # Check if there is a queen in the same column
+    for i in range(row):
+        if board[i] == col:
+            return False
+    
+    # Check upper left diagonal
+    for i, j in zip(range(row-1, -1, -1), range(col-1, -1, -1)):
+        if board[i] == j:
+            return False
+    
+    # Check upper right diagonal
+    for i, j in zip(range(row-1, -1, -1), range(col+1, len(board))):
+        if board[i] == j:
+            return False
+    
+    return True
+
+def solve_nqueens(board, row):
+    if row == len(board):
+        print([[i, board[i]] for i in range(len(board))])
+        return
+
+    for col in range(len(board)):
+        if is_safe(board, row, col):
+            board[row] = col
+            solve_nqueens(board, row + 1)
+            board[row] = -1
+
+def nqueens(N):
+    if not isinstance(N, int):
+        print("N must be a number")
+        sys.exit(1)
+    
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    
+    board = [-1] * N
+    solve_nqueens(board, 0)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    
+    nqueens(N)
+
