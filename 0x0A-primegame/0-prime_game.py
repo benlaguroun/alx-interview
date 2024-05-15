36042976affc18823e03c50346455a04cfde9df3
benlaguroun <benlaguroun@gmail.com>
2024-05-15 13:15:16 -0700

    task  0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,53 +1,70 @@
 #!/usr/bin/python3
-"""
-Solution to the Prime Game problem
+
+""" 
+Algorithm to determine the winner of the Prime Game in Python
 """
 
-def primes(n):
-    """
-    Return a list of prime numbers between 1 and n (inclusive).
+def is_prime(n):
+    """ 
+    Checks if a given number n is a prime number.
     
     Args:
-        n (int): Upper boundary of the range. The lower boundary is always 1.
-        
-    Returns:
-        list: List of prime numbers.
-    """
-    primes_list = []
-    sieve = [True] * (n + 1)
-    for p in range(2, n + 1):
-        if sieve[p]:
-            primes_list.append(p)
-            for i in range(p, n + 1, p):
-                sieve[i] = False
-    return primes_list
+        n (int): The number to check for primality.
 
-def determine_winner(num_rounds, round_limits):
+    Returns:
+        bool: True if n is prime, False otherwise.
+    """
+    for i in range(2, int(n ** 0.5) + 1):
+        if not n % i:
+            return False
+    return True
+
+def calculate_primes(n, primes):
+    """ 
+    Calculates all prime numbers up to n and stores them in the given list.
+
+    Args:
+        n (int): The upper limit for calculating prime numbers.
+        primes (list): A list to store the calculated prime numbers.
+    """
+    top_prime = primes[-1]
+    if n > top_prime:
+        for i in range(top_prime + 1, n + 1):
+            if is_prime(i):
+                primes.append(i)
+            else:
+                primes.append(0)
+
+def isWinner(x, nums):
     """
     Determines the winner of the Prime Game.
-    
+
     Args:
-        num_rounds (int): Number of rounds in the game.
-        round_limits (list): Upper limits of range for each round.
-        
+        x (int): The number of rounds in the game.
+        nums (list): An array of upper limits for each round.
+
     Returns:
-        str: Name of the winner (Maria or Ben), or None if the winner cannot be determined.
+        str: The name of the player that won the most rounds (Maria or Ben).
+        None: If the winner cannot be determined.
     """
-    if num_rounds is None or round_limits is None or num_rounds == 0 or not round_limits:
-        return None
-    
-    maria_score = ben_score = 0
-    for limit in round_limits:
-        prime_numbers = primes(limit)
-        if len(prime_numbers) % 2 == 0:
-            ben_score += 1
+    players_wins = {"Maria": 0, "Ben": 0}
+    primes = [0, 0, 2]
+    calculate_primes(max(nums), primes)
+
+    for round_num in range(x):
+        sum_options = sum((i != 0 and i <= nums[round_num]) for i in primes[:nums[round_num] + 1])
+        if sum_options % 2:
+            winner = "Maria"
         else:
-            maria_score += 1
-            
-    if maria_score > ben_score:
-        return 'Maria'
-    elif ben_score > maria_score:
-        return 'Ben'
-    else:
-        return None
+            winner = "Ben"
+
+        if winner:
+            players_wins[winner] += 1
+
+    if players_wins["Maria"] > players_wins["Ben"]:
+        return "Maria"
+    elif players_wins["Ben"] > players_wins["Maria"]:
+        return "Ben"
+
+    return None
 
